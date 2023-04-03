053cb02c97e2f8db106374aa9f10192472c32a5c
Isdhruv <dhruvpatelldhruvpatel01669@gmail.com>
2023-04-03 23:18:54 +0530

    intial-commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "../styles/Main";
+
+const Header = () => {
+  return (
+    <div>
+      <Container>
+        <h1>Calculator App</h1>
+      </Container>
+    </div>
+  );
+};
+
+export default Header;
